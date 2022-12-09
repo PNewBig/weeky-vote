7a8a391d292479d1166ab60891714deaa0e4fe13
Vanlakhan02 <100562151+Vanlakhan02@users.noreply.github.com>
2022-12-09 22:55:01 +0700

    commit vai

--- lib/Presentation/Screen/RegisterScreen.dart
+++ lib/Presentation/Screen/RegisterScreen.dart
@@ -8,55 +8,95 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-            decoration:
-                BoxDecoration(image: DecorationImage(image: AssetImage("asset/Image/background.jpeg"), fit: BoxFit.cover)),
-            child: Form(child: Container(
-              margin: const EdgeInsets.only(top: 100),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("asset/Image/background.jpeg"),
+                    fit: BoxFit.cover)),
+            child: Form(
+                child: Container(
+              margin: const EdgeInsets.only(top: 200),
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(0)),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20), topRight: Radius.circular(0)),
               ),
               child: SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                  BuildForm("Firstname", context),
-                  BuildForm("Lastname", context),
-                  BuildForm("Email", context),
-                  BuildForm("Password", context)
-                ]),
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BuildForm("Firstname", context),
+                      BuildForm("Lastname", context),
+                      BuildForm("Email", context),
+                      BuildForm("Password", context),
+                      BuildDropDown("Position", context)
+                    ]),
               ),
             ))));
+  }
+
+  Widget BuildDropDown(String text, BuildContext ctx) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      elevation: 10,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              margin: const EdgeInsets.only(left: 10, top: 10),
+              child: Text(text, style: Theme.of(ctx).textTheme.bodySmall)),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+            decoration: BoxDecoration(
+                color: Colors.grey, borderRadius: BorderRadius.circular(10)),
+            width: double.infinity,
+            child: DropdownButton<String>(
+                underline: SizedBox(),
+                items: [
+                  DropdownMenuItem(
+                      child: Text("front-end"), value: "front-end"),
+                  DropdownMenuItem(child: Text("back-end"), value: "back-end")
+                ],
+                onChanged: (value) {}),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget BuildForm(String text, BuildContext ctx) {
     return SizedBox(
       child: Card(
-        margin: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 10),
-        elevation: 10,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-        Container(
-          margin: const EdgeInsets.only(left: 10, top: 10),
-          child: Text(text, style: Theme.of(ctx).textTheme.bodySmall)),
-        SizedBox(height: 10),
-        Container(
-          decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(10)),
-          margin: const EdgeInsets.only(left:10, right: 10, bottom: 10),
-          child: TextFormField(
-              validator: (value) {
-      
-          },
-          decoration: InputDecoration(
-          contentPadding: const EdgeInsets.only(left: 10),
-          border: InputBorder.none),
-          ),
-        )
-      ])),
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          elevation: 10,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+                margin: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(text, style: Theme.of(ctx).textTheme.bodySmall)),
+            SizedBox(height: 10),
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10)),
+                margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: TextFormField(
+                  validator: (value) {},
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.only(left: 10),
+                      border: InputBorder.none),
+                )),
+
+                Container(
+                  margin: const EdgeInsets.only(top: 100),
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: (){}, child: Text("Sign in")))
+          ])),
     );
   }
 }
