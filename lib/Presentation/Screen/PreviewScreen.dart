06785246f5f76c6d84338aa2a5a6430f65533aaf
Vanlakhan02 <chanthakhaninsyxiengmai@gmail.com>
2022-12-09 18:16:44 +0700

    first commit

---
+++ lib/Presentation/Screen/PreviewScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PreviewScreen extends StatefulWidget {
+  const PreviewScreen({super.key});
+
+  @override
+  State<PreviewScreen> createState() => _PreviewScreenState();
+}
+
+class _PreviewScreenState extends State<PreviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
