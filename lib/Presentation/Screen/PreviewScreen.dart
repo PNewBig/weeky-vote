dcdb9c27793d1027504759a52c482f7e8da5e986
Mr.Phonpasit Bounkham <newbig6622@gmail.com>
2022-12-10 14:10:34 +0700

    git changed

--- lib/Presentation/Screen/PreviewScreen.dart
+++ lib/Presentation/Screen/PreviewScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weeky_vote/Presentation/widget/BuildForm.dart';
 
 class PreviewScreen extends StatefulWidget {
   static const String routeName = "/PreviewScreen";
@@ -9,13 +10,14 @@ class PreviewScreen extends StatefulWidget {
   State<PreviewScreen> createState() => _PreviewScreenState();
 }
 
-void CommentBottomSheet(BuildContext ctx){
-  showModalBottomSheet(context: ctx, builder: (context){
-    return Container(
-      height: 500,
-      child: Text("BottomSheet"));
-  });
+void CommentBottomSheet(BuildContext ctx) {
+  showModalBottomSheet(
+      context: ctx,
+      builder: (context) {
+        return Container(height: 500, child: Text("BottomSheet"));
+      });
 }
+
 class _PreviewScreenState extends State<PreviewScreen> {
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,7 @@ class _PreviewScreenState extends State<PreviewScreen> {
       body: Container(
           child: Column(
         children: [
-                    Container(
+          Container(
               width: double.infinity,
               height: 200,
               decoration: BoxDecoration(
@@ -31,27 +33,76 @@ class _PreviewScreenState extends State<PreviewScreen> {
                   borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(20),
                       bottomRight: Radius.circular(20))),
-                        child:Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                          Container(
-                            height: 100,
-                            width: 100,
-                            child: Card(
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                              child: Image(image: AssetImage("asset/Image/comment.png")))),
-                          SizedBox(height: 10),
-                          Container(
-                            height: 50,
-                            width: 150,
-                            child: Card(child: Center(child: Text(widget.name, style: Theme.of(context).textTheme.bodyText1)),))
-                        ],)
-                      ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 100,
+                      width: 100,
+                      child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Image(
+                              image: AssetImage("asset/Image/comment.png")))),
+                  SizedBox(height: 10),
+                  Container(
+                      height: 50,
+                      width: 150,
+                      child: Card(
+                        child: Center(
+                            child: Text(widget.name,
+                                style: Theme.of(context).textTheme.bodyText1)),
+                      ))
+                ],
+              )),
         ],
       )),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        CommentBottomSheet(context);
-      }, child: Icon(Icons.rate_review_outlined)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // CommentBottomSheet();
+            showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return SizedBox(
+                    child: Column(
+                      children: [
+                        Center(child: BuildForm(text: "1.Comments")),
+                        Center(
+                            child: Card(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                    margin: const EdgeInsets.only(
+                                        left: 10, top: 10),
+                                    child: Text("2.Point",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall)),
+                                SizedBox(height: 10),
+                                Container()
+                              ]),
+                        )),
+                        Center(
+                          child: Card(
+                            child: Container(
+                              child: Text("Save",
+                                  style:
+                                      Theme.of(context).textTheme.bodyMedium),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                });
+          },
+          child: Icon(Icons.rate_review_outlined)),
     );
   }
 }
